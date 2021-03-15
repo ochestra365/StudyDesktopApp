6310af74f2bce3ad3e7a711be3495c2e7b279679
ochestra365 <ochestra365@naver.com>
2021-03-15 14:27:22 +0900

    wpf DB로그인

--- WPFApp/WpfAdvBank/WpfLoginApp/LoginWindow.xaml.cs
+++ WPFApp/WpfAdvBank/WpfLoginApp/LoginWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +21,8 @@ namespace WpfLoginApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        string connString = "Data Source=127.0.0.1;Initial Catalog=PMS;Persist Security Info=True;" +
+            "User ID=sa; Password=mssql_p@ssw0rd!";//연결 문자열이 가장 중요하다.
         public MainWindow()
         {
             InitializeComponent();
@@ -44,8 +47,35 @@ namespace WpfLoginApp
 
         private void BtnLogin_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("로그인처리");
+            using (SqlConnection conn = new SqlConnection(connString))//sql커넥션
+            {
+                if (conn.State == System.Data.ConnectionState.Closed)//sql커맨드
+                {
+                    conn.Open();
+                }
+                try
+                {
+                    string query = $"SELECT count(*) " +
+                                   $"  FROM Member" +
+                                   $" WHERE UserId = '{TxtUserid.Text}' " +
+                                   $"   AND Password = '{TxtPassword.Password}'; ";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    var result = Convert.ToInt32(cmd.ExecuteScalar());
+                    //넣을 수 있는 값이 object밖에 되지않는다.
+                    //그래서 스트링으로 object로 돌려받을 때는 parameter를 Object로 쓴다.
+                    //쿼리문 날릴 떄, 띄어쓰기 매우 중요하다.
+                    if (result == 1)
+                        MessageBox.Show("로그인 성공");
+                    else
+                        MessageBox.Show("로그인 실패");
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show($"예외발생 : {ex}");
+                    return;
+                }
+            }
         }
     }
 }
-//Data Source=127.0.0.1;Initial Catalog=PMS;Persist Security Info=True;User ID=sa
