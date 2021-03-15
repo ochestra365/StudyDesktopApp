2c6d1ce3752c51a89e54d25f3b851299a62f01ab
ochestra365 <ochestra365@naver.com>
2021-03-15 13:52:38 +0900

    데이터베이스 동기화

---
+++ WPFApp/WpfAdvBank/WpfLoginApp/LoginWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfLoginApp
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            TxtUserid.Focus();
+        }
+
+        private void TxtUserid_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                TxtPassword.Focus();
+        }
+
+        private void TxtPassword_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                BtnLogin_Click(sender, e);
+        }
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("로그인처리");
+        }
+    }
+}
+//Data Source=127.0.0.1;Initial Catalog=PMS;Persist Security Info=True;User ID=sa
