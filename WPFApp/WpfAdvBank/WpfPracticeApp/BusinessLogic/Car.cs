176597889c14a8da386d5f20d9294a63860c750e
ochestra365 <ochestra365@naver.com>
2021-03-22 14:31:43 +0900

    프로퍼티에 대한 공부

--- WPFApp/WpfAdvBank/WpfPracticeApp/BusinessLogic/Car.cs
+++ WPFApp/WpfAdvBank/WpfPracticeApp/BusinessLogic/Car.cs
@@ -7,9 +7,26 @@ using System.Windows.Media;
 
 namespace WpfPracticeApp.BusinessLogic
 {
-    public class Car
+    public class Car : Notifier
     {
-        public double Speed { get; set; }
+        // public double Speed{ get; set; } 아래와 똑같음.
+        private double speed;//멤버변수
+        public double Speed {//속성
+            get { return speed; }
+            set 
+            {
+                if (value > 350)
+                {
+                    speed = 350;
+                }
+                else
+                {
+                    speed = value;
+                }
+                speed = value;
+                OnPropertyChanged("Speed");//속성값 변경된 것을 알려줌(프로그램)
+            } //부가적으로 값을 받아서 처리할 부분이 존재할 때 쓰는 Property이다.
+        }
         public Color MainColor { get; set; }//시스템함수와 충돌나지 않게 쓰는 것이 좋다.
         public Human Driver { get; set; }
     }
--- WPFApp/WpfAdvBank/WpfPracticeApp/Discussion.xaml.cs
+++ WPFApp/WpfAdvBank/WpfPracticeApp/Discussion.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfPracticeApp.BusinessLogic;
 
 namespace WpfPracticeApp
 {
@@ -23,6 +24,18 @@ namespace WpfPracticeApp
         public Discussion()
         {
             InitializeComponent();
+        }
+
+        private void Page_Initialized(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Page_Initialized_1(object sender, EventArgs e)
+        {
+            //runtime 데이터를 바인딩하는 동적바인딩.
+            Talk talk = new Talk();
+            this.DataContext = talk;
         }
     }
 }
