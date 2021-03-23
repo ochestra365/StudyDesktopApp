63648e137439a0711f256e5bb4fbc6ee57ae604b
ochestra365 <ochestra365@naver.com>
2021-03-23 14:32:09 +0900

    중간 커밋

--- WPFApp/WpfAdvBank/PhotoResisterMonApp/MainWindow.xaml.cs
+++ WPFApp/WpfAdvBank/PhotoResisterMonApp/MainWindow.xaml.cs
@@ -17,9 +17,26 @@ using LiveCharts;
 using LiveCharts.Wpf;
 using System.Threading;
 using System.Windows.Threading;
+using System.Data.SqlClient;
+using System.Diagnostics;
 
 namespace PhotoResisterMonApp
 {
+    public class SensorData
+    {
+        public int Idx { get; set; }
+        public DateTime CurrentDt { get; set; }
+        public int Value { get; set; }
+        public bool SimulFlag { get; set; }
+
+        public SensorData(int idx, DateTime currentDt, int value, bool simulFlag)
+        {
+            Idx = idx;
+            CurrentDt = currentDt;
+            Value = value;
+            SimulFlag = simulFlag;
+        }
+    }
     /// <summary>
     /// MainWindow.xaml에 대한 상호 작용 논리
     /// </summary>
@@ -35,6 +52,50 @@ namespace PhotoResisterMonApp
         public int SensorValue { get; set; }
 
         public DispatcherTimer CustomTimer { get; set; }
+
+        private string connString = "Data Source=127.0.0.1;" +
+                                    "Initial Catalog=IoTData;" +
+                                    "Persist Security Info=True;" +
+                                    "User ID=sa;Password=mssql_p@ssw0rd!";
+
+        public SensorData GetRealTimeSensor()
+        {
+
+            SensorData result = null;
+            
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connString))
+                {
+                    if (conn.State == System.Data.ConnectionState.Closed) conn.Open();
+
+                    var query = @"SELECT TOP 1 Idx
+                                          ,CurrentDt
+                                          ,Value
+                                          ,SimulFlag
+                                      FROM Tbl_PhotoResister
+                                      ORDER BY Idx DESC;";
+
+                    SqlCommand cmd = new SqlCommand(query, conn);//conn에 있는 쿼리를 사용하겠다.
+                    var temp = cmd.ExecuteReader();
+                    if (temp.Read())
+                    {
+                        result = new SensorData(Convert.ToInt32(temp["Idx"]), 
+                                                Convert.ToDateTime(temp["CurrentDt"]),
+                                                Convert.ToInt32( temp["Value"]),
+                                                Convert.ToBoolean(temp["SimulFlag"]));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"예외발생: {ex.Message}");
+            }
+
+            return result;
+        }
+
+
         private void Window_Initialized(object sender, EventArgs e)
         {
             //var x = Enumerable.Range(0, 1001).Select(i => i / 10.0).ToArray();
@@ -46,13 +107,32 @@ namespace PhotoResisterMonApp
             CustomTimer = new DispatcherTimer();
             CustomTimer.Interval = TimeSpan.FromSeconds(1);
             CustomTimer.Tick += CustomTimer_Tick;
+            //CustomTimer.Start();
+        }
+
+        private void CustomTimer_Tick(object sender, EventArgs e)//db 연동
+        {
+            GrdRealTime.DataContext = GetRealTimeSensor().Value;//new Random().Next(0, 1023);
+        }
+
+        private void MnuStart_Click(object sender, RoutedEventArgs e)
+        {
             CustomTimer.Start();
         }
 
-        private void CustomTimer_Tick(object sender, EventArgs e)
+        private void MnuStop_Click(object sender, RoutedEventArgs e)
         {
-            SensorValue = new Random().Next(0, 1023);
-            GrdRealTime.DataContext = SensorValue;
+            CustomTimer.Stop();
+        }
+
+        private void MnuExit_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void MnuLoad_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
